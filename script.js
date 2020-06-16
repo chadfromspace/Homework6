da116dc6181a8c2abbc68a6cf22e5ea483c9b78a
imac <chadmdubay@gmail.com>
2020-06-16 19:01:24 -0400

    Backup.

--- script.js
+++ script.js
@@ -62,7 +62,6 @@ $(document).ready(function(){
     */
     $(".searchButton").on("click",function(event){
         event.preventDefault();
-        console.log("Yes");
         searchInput = event.target.innerHTML;
         searchInputArray.push(searchInput);
         var latestCity = [];     
@@ -156,7 +155,52 @@ $(document).ready(function(){
         })
     })
 
-    function getWeatherInfo(){
-        
-    }
+    $(".searchIcon").on("click",function(event){
+        event.preventDefault();
+        var searchInput = event.currentTarget.parentElement.children[0][0].value;
+        searchInputArray.push(searchInput);
+        var latestCity = [];
+        var newButton = document.createElement("button");
+        newButton.textContent = searchInput;
+        $(newButton).addClass("btn btn-dark col-12");
+        $(newForm).prepend(newButton);        
+        localStorage.setItem("searchHistory",searchInputArray);
+        $.ajax({
+            url: "https://api.openweathermap.org/data/2.5/weather?q="+searchInput+"&units=imperial&appid=da0d8ead8e11a7c6ac8547cdc2d96e73",
+            method: "get"
+        }).then(function(response){            
+            $(".cityTemperature").html("Temperature: "+response.main.temp+" &degF");
+            $(".humidity").html("Humidity: "+response.main.humidity+"%");
+            $(".windSpeed").html("Wind Speed: "+response.wind.speed+" MPH");
+            //$(".uvIndex").attr("src","http://openweathermap.org/img/w/" + response.weather[0].icon +".png");
+            longitude = response.coord.lon;
+            latitude = response.coord.lat;
+        }).then(function(response){
+            $.ajax({
+                url: "https://api.openweathermap.org/data/2.5/uvi?appid=da0d8ead8e11a7c6ac8547cdc2d96e73&lat="+latitude+"&lon="+longitude,
+                method: "get"
+            }).then(function(response){
+                $(".uvIndex").html("UV Index: "+response.value);
+                localStorage.setItem("latestCity",latestCity);
+                }).then(function(response){
+                    $.ajax({
+                        url: "https://api.openweathermap.org/data/2.5/forecast?q="+searchInput+"&appid=da0d8ead8e11a7c6ac8547cdc2d96e73",
+                        method: "get"
+                    }).then(function(response){
+                        var currentCity1 = searchInput;
+                        var currentCityArray = [];
+                        for(i=0;i<currentCity1.length;i++){
+                            if(i===0){
+                                currentCityArray.push(currentCity1[i].toUpperCase());
+                            } else{
+                                currentCityArray.push(currentCity1[i].toLowerCase());
+                            }
+                        }
+                        var currentCity2 = currentCityArray.join(",").replace(/,/g,"")
+                        var currentDay = response.list[0].dt_txt.replace(/-/g,"/").split(" ")[0];
+                        $(".currentCity").html(currentCity2+" ("+currentDay+")");
+                    })
+            })
+        })
+    })
 })
